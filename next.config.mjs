7262c6ed415f2955b858cb594908febf35910f7e
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2026-01-05 21:15:20 +0530

    fix prod ai rag error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  monitor: false,
+  turbopack: {
+    resolveAlias: {
+      "onnxruntime-node": "onnxruntime-web",
+      sharp: {
+        browser: "empty-module", // Mock for browser
+      }
+    },
+  },
   webpack: (config) => {
     // Ignore node-specific modules when bundling for the browser
     // https://webpack.js.org/configuration/resolve/#resolvealias
